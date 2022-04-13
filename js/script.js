e5d0aa8a32c64b111bf5fe4573ce4122ca5d48c0
Petrus <daffa@MacBook-Air-Rz.local>
2022-04-13 21:23:13 +0700

    dark mode

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const card = document.getElementsByClassName('card')[0]
 const toggleElements = document.querySelectorAll('.nav-list a');
 
 toggleElements.forEach(el => {
-    el.addEventListener('click', () => {
+    el.addEventListener('click', () => {    
         card.style.display = 'none';
     });
 });
